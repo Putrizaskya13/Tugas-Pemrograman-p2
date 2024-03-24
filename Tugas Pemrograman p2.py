6f04263bf4b6beced8dc7dfe341d3e97e609f2bc
Putrizaskya13 <ptriizaskya13@gmail.com>
2024-03-24 14:00:14 +0700

    Tugas Pemrograman p2

---
+++ Tugas Pemrograman p2.py
@@ -0,0 +1,26 @@
+#Menghitung Luas Segitiga
+
+A = int(input( 'Masukkan Alas : '))
+T = int(input( 'Masukkan Tinggi : '))
+
+L_Segitiga = 0.5 * A * T
+
+print('Luas Segitiga Adalah : ',L_Segitiga)
+
+
+
+#Menghitung Luas Lingkaran
+
+r = int(input( 'Masukkan Jari2 : '))
+L_Lingkaran = 22/7 * r * r
+Luas = 3.14 * r * r
+print('Luas Lingkaran Adalah : ',L_Lingkaran,Luas)
+
+
+#Menghitung Luas Persegi 
+
+s = int(input('Masukkan Sisi : '))
+L_Persegi = s * s
+print('Luas Persegi : ',L_Persegi)
+
+
