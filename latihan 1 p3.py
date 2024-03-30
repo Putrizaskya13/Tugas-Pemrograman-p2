4ec6e29f7dbedae6339563763af2ff219e6b99cf
Putrizaskya13 <ptriizaskya13@gmail.com>
2024-03-30 17:13:56 +0700

    Tugas Pemrograman

---
+++ latihan 1 p3.py
@@ -0,0 +1,16 @@
+print('                                TOKO MAINAN ANAK                               ')
+print('                              ********************                             ')
+
+NamaPembeli = input("Masukkan Nama Pembeli :")
+KodeMainan = input("Masukan Kode Mainan :")
+Harga = int(input('Masukkan Harga = '))
+JumlahBeli = int(input('Masukkan Jumlah Beli = '))
+Total = Harga * JumlahBeli
+print('================================================')
+print('Nama Pembeli =',NamaPembeli)
+print('Kode Kue =', KodeMainan)
+print('Harga =', Harga)
+print('Jumlah Beli =', JumlahBeli)
+print('Total =', Total)
+
+
---
+++ latihan p3.py
@@ -0,0 +1,12 @@
+var1 = 'Hello World'
+var2 = 'I Love Python'
+print('var1[0]', var1[0])
+print('var2[0]:', var2[5:6])
+
+var3 = 'Hello World'
+var4 = var3[6:11]
+print('Data update:', var4 + 'Python')
+
+str1 = "hello"
+str2 = "python"
+print(str1 + " " + str2)
